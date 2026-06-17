2a73d46e43578da1cea4e17d8f30e6e1aad8a9ce
Ahda Rindang Al-Amin <ahdaalamin2506@gmail.com>
2026-06-17 09:37:34 +0700

    Fix Seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@adzkia.ac.id',
+            'password' => Hash::make('password123'),
+            'role' => 'super_admin',
+            'divisi' => 'Pusat',
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,32 +2,20 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            ProdiSeeder::class,
+            PembayaranSeeder::class,
+            TugasAdminSeeder::class,
         ]);
-        \App\Models\User::create([
-        'name' => 'Super Admin',
-        'email' => 'admin@adzkia.ac.id',
-        'password' => \Illuminate\Support\Facades\Hash::make('password123'), // Password Anda
-        'role' => 'super_admin',
-        'divisi' => 'Pusat',
-    ]);
     }
-    
 }
