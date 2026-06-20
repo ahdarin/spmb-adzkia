5e4d52d8e44c9ca92f6f61a793e6ad0cdc21229b
Ahda Rindang Al-Amin <ahdaalamin2506@gmail.com>
2026-06-20 22:25:57 +0700

    added SoalKuesionerSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProdiSeeder::class,
+            SoalKuesionerSeeder::class,
             // PendaftarSeeder::class,
             // PembayaranSeeder::class,
             // TugasAdminSeeder::class,
